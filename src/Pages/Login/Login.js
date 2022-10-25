fb90e75e77c39b7c2637eee6543294039b8d6c22
Anas <anasmahmud523@gmail.com>
2022-10-25 21:55:50 +0600

    register route

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,49 +3,34 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 my-28">
-            <div class="w-full max-w-md">
-                <div>
-                    <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-                    <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-                </div>
-                <form class="mt-8 space-y-6" action="#" method="POST">
-                    <input type="hidden" name="remember" value="true" />
-                    <div class="-space-y-px rounded-md shadow-sm">
-                        <div className='mb-2'>
-                            <label for="email-address" class="sr-only">Email address</label>
-                            <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
-                        </div>
-                        <div>
-                            <label for="password" class="sr-only">Password</label>
-                            <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
-                        </div>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-                            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-                        </div>
-
-                        <div class="text-sm">
-                            <Link href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</Link>
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                                {/* <!-- Heroicon name: mini/lock-closed --> */}
-                                <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-                                </svg>
-                            </span>
-                            Sign in
-                        </button>
-                    </div>
-                </form>
+        <div className="flex flex-col max-w-lg p-6 rounded-md sm:p-10 mx-auto my-20 dark:bg-gray-900 dark:text-gray-100">
+            <div className="mb-8 text-center">
+                <h1 className="my-3 text-4xl font-bold">Sign in</h1>
+                <p className="text-sm dark:text-gray-400">Sign in to access your account</p>
             </div>
+            <form novalidate="" action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
+                <div className="space-y-4">
+                    <div>
+                        <label for="email" className="block mb-2 text-sm">Email address</label>
+                        <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                    </div>
+                    <div>
+                        <div className="flex justify-between mb-2">
+                            <label for="password" className="text-sm">Password</label>
+                            <Link rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">Forgot password?</Link>
+                        </div>
+                        <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                    </div>
+                </div>
+                <div className="space-y-2">
+                    <div>
+                        <button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">Sign in</button>
+                    </div>
+                    <p className="px-6 text-sm text-center dark:text-gray-400">Don't have an account yet?
+                        <Link rel="noopener noreferrer" href="#" className="hover:underline dark:text-violet-400">Sign up</Link>.
+                    </p>
+                </div>
+            </form>
         </div>
     );
 };
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 export const routes = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ export const routes = createBrowserRouter([
                 path: '/login',
                 element: <Login></Login>
             },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
         ]
     }
 ])
