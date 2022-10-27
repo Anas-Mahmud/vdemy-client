a07adc11da2fdef098739c1587e771e175d19cc1
Anas <anasmahmud523@gmail.com>
2022-10-27 22:14:08 +0600

    modify

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -8,8 +8,8 @@ const Courses = () => {
     return (
         <div>
             <div className="space-y-2 text-center mt-10">
-                <h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
-                <p className="font-serif text-sm dark:text-gray-400">Qualisque erroribus usu at, duo te agam soluta mucius.</p>
+                <h2 className="text-3xl font-bold">Our premium courses content</h2>
+                <p className="font-serif text-sm dark:text-gray-400">Take one of Vdemys range of Javascript courses and learn how to code using this incredibly useful language.</p>
             </div>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 p-10'>
                 {
